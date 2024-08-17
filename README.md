fcbba73785b73d1196a526497e039d9f4f968364
Chris Gastaldo <chris.gastaldo@gmail.com>
2024-08-16 21:34:12 -0400

    Cleaned createGrid function and implemented replacing current grid. Finished project

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # EtchSketch
+
+Project Odin Foundations Etch-a-Sketch.
+
+Minimized amount of CSS used and pushed majority through js file. Could eliminate CSS file but completed goal and don't see point.
+
+Project took a while - stuck on elimination of current grid to replace with new grid. The divs were apart of an array and could not be deleted individually. 
--- etchsketch.js
+++ etchsketch.js
@@ -1,27 +1,6 @@
 const body = document.body;
-body.id ='grid';
+body.id ='id';
 
-function combine(str1, str2){
-    return str1.concat(str2);
-}
-/*
-function setColor(blockSize){
-    this.setAttribute('style', 'width:' + String(blockSize) +
-                'px;' + 'height:' + String(blockSize) + 'px;' +
-                'margin:1px; background-color:rgb(15, 150, 150);')
-}
-
-function setStyle(size, bgColor){
-    if (bgColor === undefined){
-        bgColor = black;
-    }
-    this.setAttribute('style', 'width:' + String(size) +
-                'px;' + 'height:' + String(size) + 'px;' +
-                'margin:1px; background-color:' + bgColor + ';')
-}
-*/
-
-//New Grid Button
 const sizeBtn = document.createElement('button');
 sizeBtn.id = 'gridBtn';
 sizeBtn.innerText='Grid Size';
@@ -35,56 +14,41 @@ window.onload=function(){
             alert('Must select value lower than 100')
             return
         }
-        block = getblockSize(size);
-        createGrid(size, block);
-    }
-}
-
-function getblockSize(gridSize){
-    return 680/gridSize -2;
-}
-//NEED TO GET INITIAL GRID TO GO AWAY
-function createGrid(gridSize, blockSize, defaultGrid){
-    if (defaultGrid === undefined){
-        defaultGrid = false;
-    }
-    let columns = rows = gridSize;
-    for (i=0; i<rows; i++){
-        let parentDiv = combine('div',i);
-        parentDiv = document.createElement('div');
-        parentDiv.className = 'rows';
-        body.appendChild(parentDiv);
-        for (j=0; j<columns; j++){
-            let childDiv = parentDiv + String(j);
-            childDiv = document.createElement('div');
-            childDiv.className ='columns';
-            
-            if (defaultGrid == true){
-                childDiv.setAttribute('style', 'width:' + String(blockSize) +
-                    'px;' + 'height:' + String(blockSize) + 'px;' +
-                    'margin:1px; background-color:black;');
-                parentDiv.appendChild(childDiv);
-            }
-            else{
-                childDiv.setAttribute('style', 'width:' + String(10) +
-                    'px;' + 'height:' + String(10) + 'px;' +
-                    'margin:1px; background-color:black;');
-                parentDiv.appendChild(childDiv);
-            }
-
-            childDiv.addEventListener('mouseover', function(){
-                this.setAttribute('style', 'width:' + String(blockSize) +
-                'px;' + 'height:' + String(blockSize) + 'px;' +
-                'margin:1px; background-color:rgb(15, 150, 150);')
-            });
+        //removes current Divs
+        elementArray = document.getElementsByClassName('block');
+        elementArray = [].slice.call(elementArray,0);
+        for(i=0; i<elementArray.length; i++){
+            elementArray[i].remove();
         }
-    }
-
+        createGrid(size);
+        }
 }
 
-defaultBlockSize = getblockSize(16);
-createGrid(16, defaultBlockSize, true)
+const gridDiv = document.createElement('div');
+gridDiv.id = 'grid';
+body.append(gridDiv)
 
+function getBlockSize(gridSize){
+    return 600/gridSize;
+}
 
+function setColor(){
+    this.style.backgroundColor = 'rgb(15, 150, 150)';
+}
 
+function createGrid(gridSize){
+    let columns = rows = gridSize;
+    blockSize = getBlockSize(rows);
+    console.log(blockSize)
+    for (i=0; i<rows*columns; i++){
+        let blockDiv = document.createElement('div');
+        blockDiv.className = 'block';
+        blockDiv.style.backgroundColor = 'black';
+        blockDiv.style.height = String(blockSize) + 'px';
+        blockDiv.style.width = String(blockSize) + 'px';
+        gridDiv.append(blockDiv);
+        blockDiv.addEventListener('mouseover', setColor);
+    }
+}
 
+createGrid(16)
